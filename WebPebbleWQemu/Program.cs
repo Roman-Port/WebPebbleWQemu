922c36eb4c4816d2337595f477b52d519bbc779d
Roman-Port <darkwings220@gmail.com>
2018-11-30 16:29:42 -0600

    Initial commit

---
+++ WebPebbleWQemu/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebPebbleWQemu
+{
+    class Program
+    {
+        /// <summary>
+        /// Provides control for QEMU and a proxy bridge for VNC, all via WebSockets.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting WebPebble WQemu...");
+        }
+    }
+}
